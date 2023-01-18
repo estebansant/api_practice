ff115dab688207e08357e36bad505d91e5b4d032
estebansant <estebansant30@gmail.com>
2023-01-18 14:37:04 -0300

    added more api images to the display

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-console.log("hello world");
-
-const url = 'https://api.thedogapi.com/v1/images/search';
+const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3';
 const button = document.getElementById("button");
-const img = document.getElementById("image");
+const img1 = document.getElementById("image1");
+const img2 = document.getElementById("image2");
+const img3 = document.getElementById("image3");
 
 async function loadDog () {
-    const request = await fetch(url);
-    const response = await request.json();
-    img.src = response[0].url
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+
+    img1.src = data[0].url
+    img2.src = data[1].url
+    img3.src = data[2].url
 }
 
 button.onclick = loadDog;
+
+loadDog();
